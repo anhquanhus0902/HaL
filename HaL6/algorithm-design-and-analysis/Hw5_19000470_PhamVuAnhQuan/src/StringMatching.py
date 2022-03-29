97768570777c1bb32ad3d55bf3f7a1b7db69dd4b
anhquanhus0902 <pvaquans22@gmail.com>
2022-03-29 21:56:47 +0700

    hw4-modify & hw5

---
+++ HaL6/algorithm-design-and-analysis/Hw5_19000470_PhamVuAnhQuan/src/StringMatching.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import traceback
+
+def isValidFilePath(path: str) -> bool:
+    path = path.replace('\\', '/')
+    try:
+        f = open(path, 'r')
+        f.close()
+        return True
+    except:
+        return False
+
+def getInput() -> tuple[list, str]:
+    textData = input('(if you enter a valid file path, you can keep it as text data or get text data from the file with that path)\nEnter the text data:\n')
+    if isValidFilePath(textData):
+        choiceSelectedByUser = input('you entered a valid file path. do you want to use text data from this file? (Y/n): ').lower()
+        if choiceSelectedByUser == 'y':
+            with open(textData) as f:
+                textData = [line.rstrip() for line in f]
+    if not type(textData) is list:
+        textData = [textData]
+    pattern = input('Enter the pattern string:\n')
+    return (textData, pattern)
+
+def solve(textData: str, pattern: str) -> tuple[int, int]:
+    lenOfTextData = len(textData)
+    m = len(pattern)
+    for i in range(lenOfTextData):
+        n = len(textData[i])
+        for j in range(n-m+1):
+            k = 0
+            while k < m and pattern[k] == textData[i][j+k]:
+                k += 1
+            if k == m:
+                return (j+1, i+1)
+    return (-1, -1)
+
+if __name__ == "__main__":
+    try:
+        textData, pattern = getInput()
+        res = solve(textData, pattern)
+        print('column (index): {}, row: {}'.format(res[0], res[1]))
+    except Exception:
+        traceback.print_exc()
