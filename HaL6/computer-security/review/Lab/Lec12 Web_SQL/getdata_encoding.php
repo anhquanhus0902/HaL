e498c346bb9e7d8df2357fed7be785afcc00d2fe
anhquanhus0902 <pvaquans22@gmail.com>
2022-06-20 17:23:41 +0700

    remove review dir

--- HaL6/computer-security/review/Lab/Lec12 Web_SQL/getdata_encoding.php
+++
@@ -1,9 +0,0 @@
-<?php
-   $conn = new mysqli("localhost", "root", "seedubuntu", "dbtest");
-   $eid = $mysqli->real_escape_string($_GET['EID']);         
-   $pwd = $mysqli->real_escape_string($_GET['Password'];    
-   $sql = "SELECT Name, Salary, SSN
-           FROM employee
-           WHERE eid= '$eid' and password='$pwd'";
-?>
-
